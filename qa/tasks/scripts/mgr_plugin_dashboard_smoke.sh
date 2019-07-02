538d8c8ab3fd4e757ca5e5d5e496269a53cb474e
gkyratsas <gkyratsas@suse.com>
2019-07-02 17:51:23 +0200

    Extended branding smoketest

--- qa/tasks/scripts/mgr_plugin_dashboard_smoke.sh
+++ qa/tasks/scripts/mgr_plugin_dashboard_smoke.sh
@@ -15,13 +15,18 @@ if [ ! -n "$URL" ]; then
   false
 fi
 
+
+function test_screen(){
+test -s $1.html
+file $1.html | grep "HTML document"
+# verify SUSE branding (in title and suse_favicon)
+grep -i "suse" $1.html
+echo "$1 screen OK" >/dev/null
+}
+
 # check the login screen
 curl --insecure --silent $URL 2>&1 > dashboard.html
-test -s dashboard.html
-file dashboard.html | grep "HTML document"
-# verify SUSE branding (in title and suse_favicon)
-grep -i "suse" dashboard.html
-echo "Login screen OK" >/dev/null
+test_screen dashboard
 
 # set a password for the admin user
 ceph dashboard ac-user-set-password admin admin >/dev/null
@@ -32,10 +37,34 @@ TOKEN=$(curl --insecure -s -H "Content-Type: application/json" -X POST \
             | jq -r .token)
 
 # pass the login screen
-result=$(curl --insecure -s -H "Authorization: Bearer $TOKEN " \
+curl --insecure -s -H "Authorization: Bearer $TOKEN " \
                -H "Content-Type: application/json" -X GET \
-               ${URL})
+               ${URL} 2>&1 > main.html
+test_screen main
+
+#Check the pools page
+curl --insecure -s -H "Authorization: Bearer $TOKEN " \
+               -H "Content-Type: application/json" -X GET \
+               ${URL}/\#/pool 2>&1 > pools.html
+test_screen pools	
 
 
-# script continues beyond this line - check result, issue more curl commands,
-# validate other screens ... (?)
+#check if the SUSE branded html login and 'About' files exist on the mgr node
+test -f /usr/share/ceph/mgr/dashboard/frontend/src/app/core/auth/login/login.component.brand.html
+echo "SUSE Branded HTML file OK" >/dev/null
+test -f /usr/share/ceph/mgr/dashboard/frontend/src/app/core/navigation/about/about.component.brand.html
+echo "SUSE Branded 'About' file OK" >/dev/null
+
+#Check if the SUSE logos exist on the mgr node
+declare -a arr=("suse_brand_bright.png" "suse_favicon.png" \
+                "suse_logo_footer.png" "suse_logo_login.png" \
+                "suse_logo_login.svg" "suse_logo.png" "suse_logo.svg")
+for i in "${arr[@]}"
+do
+        test -f /usr/share/ceph/mgr/dashboard/frontend/src/assets/$i
+        echo "$i file OK" >/dev/null
+done
+
+
+
+
